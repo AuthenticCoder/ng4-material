4e5e8181248852a53c197f50e003a65fac9e2b20
Imtiaz Uddin <imtiazuddinbd4@yahoo.com>
2017-09-08 06:01:23 +0600

    Introduction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+
+
+import { MyFormComponent } from './my-form/my-form.component';
+
+
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdInputModule, MdButtonModule} from '@angular/material';
+import "hammerjs";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFormComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
